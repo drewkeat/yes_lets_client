21d8803632202ecabf6491f85762fd41662ba773
Drew Keat <thedrewkeat@gmail.com>
2022-03-25 21:08:34 -1000

    Add ThemeProvider and Theme files

--- src/Containers/Landing.jsx
+++ src/Containers/Landing.jsx
@@ -1,11 +1,19 @@
-import React, { Component } from 'react'
+import { Box, Button, Typography } from "@mui/material";
+import React, { Component } from "react";
 
 export class Landing extends Component {
   render() {
     return (
-      <h1>Landing</h1>
-    )
+      <div>
+        <Typography variant="h1" align="center">
+          Yes, Lets!
+        </Typography>
+        <Box display="flex" alignContent="center" justifyContent="center">
+          <Button variant="contained">Click Me</Button>
+        </Box>
+      </div>
+    );
   }
 }
 
-export default Landing
+export default Landing;
--- src/index.js
+++ src/index.js
@@ -5,13 +5,18 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import configureStore from "./configStore";
+import { ThemeProvider } from "@mui/material";
+
+import theme from "./theme";
 
 const store = configureStore();
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")

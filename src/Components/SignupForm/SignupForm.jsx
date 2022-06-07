b3ab3b95da35d725dcf1e24fa9da1473cfc8f2fd
Drew Keat <thedrewkeat@gmail.com>
2022-06-06 22:45:43 -0600

    Change signup/login forms to functional components

--- src/Components/SignupForm/SignupForm.jsx
+++ src/Components/SignupForm/SignupForm.jsx
@@ -1,98 +1,96 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { connect } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import { createUser } from "../../Actions/userActions";
-import { withRouter } from "../../Utils/withRouter";
 
-class SignupForm extends Component {
-  state = {
+const SignupForm = ({ changeForm, createUser, ...props }) => {
+  const [state, setState] = useState({
     first: "",
     last: "",
     email: "",
     phone_number: "",
     password: "",
     password_confirmation: "",
-  };
+  });
 
-  handleChange = (e) => {
-    this.setState({ [e.target.name]: e.target.value });
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setState({ ...state, [e.target.name]: e.target.value });
   };
 
   // TODO: Alter to transmit form values to api
-  handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    this.props.createUser(this.state, this.props.navigate);
+    createUser(state, navigate);
   };
 
-  render() {
-    return (
-      <div
-        className="form-container"
+  return (
+    <div
+      className="form-container"
+      style={{
+        display: "grid",
+        justifyContent: "center",
+        width: "100vw",
+        ...props.style,
+      }}
+    >
+      <form
         style={{
           display: "grid",
+          gridTemplateColumns: "repeat(2,1fr)",
           justifyContent: "center",
-          width: "100vw",
-          ...this.props.style,
+          gap: ".25rem",
         }}
+        onSubmit={(e) => handleSubmit(e)}
       >
-        <form
-          style={{
-            display: "grid",
-            gridTemplateColumns: "repeat(2,1fr)",
-            justifyContent: "center",
-            gap: ".25rem",
-          }}
-          onSubmit={(e) => this.handleSubmit(e)}
-        >
-          <input
-            name="first"
-            type="text"
-            placeholder="First Name"
-            onChange={(e) => this.handleChange(e)}
-            value={this.state.first}
-          />
-          <input
-            name="last"
-            type="text"
-            placeholder="Last Name"
-            onChange={(e) => this.handleChange(e)}
-            value={this.state.last}
-          />
-          <input
-            name="email"
-            type="email"
-            placeholder="Email"
-            onChange={(e) => this.handleChange(e)}
-            value={this.state.email}
-          />
-          <input
-            name="phone_number"
-            type="text"
-            placeholder="Phone Number"
-            onChange={(e) => this.handleChange(e)}
-            value={this.state.phone_number}
-          />
-          <input
-            name="password"
-            type="password"
-            placeholder="Password"
-            onChange={(e) => this.handleChange(e)}
-            value={this.state.password}
-          />
-          <input
-            name="password_confirmation"
-            type="password"
-            placeholder="Confirm Password"
-            onChange={(e) => this.handleChange(e)}
-            value={this.state.password_confirmation}
-          />
-          <button type="submit">Join</button>
-          <button onClick={() => this.props.changeForm(true)}>
-            Back to Login
-          </button>
-        </form>
-      </div>
-    );
-  }
-}
+        <input
+          name="first"
+          type="text"
+          placeholder="First Name"
+          onChange={(e) => handleChange(e)}
+          value={state.first}
+        />
+        <input
+          name="last"
+          type="text"
+          placeholder="Last Name"
+          onChange={(e) => handleChange(e)}
+          value={state.last}
+        />
+        <input
+          name="email"
+          type="email"
+          placeholder="Email"
+          onChange={(e) => handleChange(e)}
+          value={state.email}
+        />
+        <input
+          name="phone_number"
+          type="text"
+          placeholder="Phone Number"
+          onChange={(e) => handleChange(e)}
+          value={state.phone_number}
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          onChange={(e) => handleChange(e)}
+          value={state.password}
+        />
+        <input
+          name="password_confirmation"
+          type="password"
+          placeholder="Confirm Password"
+          onChange={(e) => handleChange(e)}
+          value={state.password_confirmation}
+        />
+        <button type="submit">Join</button>
+        <button onClick={() => changeForm(true)}>Back to Login</button>
+      </form>
+    </div>
+  );
+};
 
-export default connect(null, { createUser })(withRouter(SignupForm));
+export default connect(null, { createUser })(SignupForm);

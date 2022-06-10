61672d45f88ebdf166593d3e978ed9709b3c2afd
Drew Keat <thedrewkeat@gmail.com>
2022-06-09 20:20:01 -0600

    Uncomment build server

--- src/Utils/constants.js
+++ src/Utils/constants.js
@@ -1,4 +1,4 @@
-// const API_URL = "http://localhost:3001";
-const API_URL = "https://yes-lets-api.herokuapp.com";
+const API_URL = "http://localhost:3001";
+// const API_URL = "https://yes-lets-api.herokuapp.com";
 
 export { API_URL };

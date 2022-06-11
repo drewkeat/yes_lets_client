3c57a1c8dbe3194e374ebd36ff26c7dc993095a5
Drew Keat <thedrewkeat@gmail.com>
2022-06-10 19:56:12 -0600

    Pass user to SmallCalendar and use display dates w/ availabilities

--- src/Components/SmallCalendar/SmallCalendar.jsx
+++ src/Components/SmallCalendar/SmallCalendar.jsx
@@ -5,19 +5,38 @@ import Calendar from "react-calendar";
 import { getAvailabilitiesFromUser } from "../../Reducers/Availabilities/AvailabilitiesSelectors";
 import "./Calendar.css";
 
-const SmallCalendar = ({ changeDate, date, user, ...props }) => {
-  useEffect(() => {}, []);
-
+const SmallCalendar = ({
+  changeDate,
+  date,
+  user,
+  availabilities,
+  loading,
+  ...props
+}) => {
   // TODO: Update to display availabilities and hangouts
-  const tileContent = ({ date, view }) =>
-    view === "month" && date.getDay() === 0 ? <p>It's Sunday!</p> : null;
 
+  const datesToCheck = availabilities.map((a) => new Date(a.attributes.start));
+
+  const tileContent = ({ date, view }) => {
+    if (view === "month" && date.getDay() === 0) {
+      return <p>It's Sunday</p>;
+    }
+  };
+
+  const tileClassName = ({ date, view }) => {
+    if (view === "month") {
+      if (datesToCheck.find((cDate) => cDate.getDate() === date.getDate())) {
+        return "has-details";
+      }
+    }
+  };
   return (
     <div style={{ width: "100%" }}>
       <Calendar
         onChange={changeDate}
         value={date}
         tileContent={tileContent}
+        tileClassName={tileClassName}
         calendarType={"US"}
       />
     </div>
@@ -26,7 +45,7 @@ const SmallCalendar = ({ changeDate, date, user, ...props }) => {
 
 const mapStateToProps = (state, { user, ...props }) => {
   return {
-    availabilites: getAvailabilitiesFromUser(state, user),
+    availabilities: getAvailabilitiesFromUser(state, user),
   };
 };
 

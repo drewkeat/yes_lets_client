120c7d946078bcfb489205ac11568d0d8df4acba
Drew Keat <thedrewkeat@gmail.com>
2022-06-17 06:56:39 -0600

    add notes

--- src/Components/SignupForm/SignupForm.jsx
+++ src/Components/SignupForm/SignupForm.jsx
@@ -117,9 +117,11 @@ const SignupForm = ({
           </>
         )}
         {user && (
-          <button style={{ gridColumn: "1/ span 2" }} type="submit">
-            Update Profile
-          </button>
+          <>
+            <button type="submit">Update Profile</button>
+            {/* TODO: Create delete profile action */}
+            <button type="button">Delete Profile</button>
+          </>
         )}
       </form>
     </div>

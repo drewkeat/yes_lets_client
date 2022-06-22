2687e3292e47d6781973d0d8a2d234ad3f996948
Drew Keat <thedrewkeat@gmail.com>
2022-06-21 21:24:04 -0600

    Stub out search field and user list logic for FriendSearch component within FriendsNav, adjust UsersSelectors to return user objects and friendIDs

--- src/Containers/FriendsNav.jsx
+++ src/Containers/FriendsNav.jsx
@@ -1,19 +1,67 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Navbar } from "../Components";
+import {
+  selectCurrentFriendIDs,
+  selectCurrentUser,
+  selectFriends,
+  selectUsers,
+} from "../Reducers/Users/UsersSelectors";
 
 class FriendsNav extends Component {
+  state = {
+    input: "",
+  };
+
+  handleChange = (e) => {
+    this.setState({ ...this.state, input: e.target.value });
+  };
+
+  renderUsers = () => {
+    if (this.state.input) {
+      return this.props.users.map((user) => {
+        let userName = user.attributes.fullName.toLowerCase();
+        if (userName.includes(this.state.input.toLowerCase())) {
+          return <li>{user.attributes.fullName}</li>;
+        }
+        return null;
+      });
+    }
+  };
+
+  renderFriends = () => {
+    return this.props.friendIDs.map((id) => {
+      let friend = this.props.users.find((user) => user.id == id);
+      return (
+        <div>
+          <h3>{friend.attributes.fullName}</h3>
+          <p>{friend.attributes.email}</p>
+        </div>
+      );
+    });
+  };
+
   render() {
     return (
       <div>
         <Navbar />
         <h1 style={{ textAlign: "center" }}>FriendsNav</h1>
+        <div>{this.renderFriends()}</div>
+        <label htmlFor="searchField">Search:</label>
+        <input type="search" name="searchField" onChange={this.handleChange} />
+
+        <h3>User List</h3>
+        <ul style={{ listStyle: "none" }}>{this.renderUsers()}</ul>
       </div>
     );
   }
 }
 
-const mapStateToProps = (state) => ({});
+const mapStateToProps = (state) => ({
+  currentUser: selectCurrentUser(state),
+  users: selectUsers(state),
+  friendIDs: selectCurrentFriendIDs(state),
+});
 
 const mapDispatchToProps = {};
 

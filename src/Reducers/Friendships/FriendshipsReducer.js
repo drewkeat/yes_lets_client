ff94ee3168d5d9f5ddbb68e96a42c6870fd82ccd
Drew Keat <thedrewkeat@gmail.com>
2022-06-28 17:32:23 -0600

    Build out FriendshipsReducer methods

--- src/Reducers/Friendships/FriendshipsReducer.js
+++ src/Reducers/Friendships/FriendshipsReducer.js
@@ -1,10 +1,17 @@
-// import ACTIONS from "../Actions/actionTypes";
+import ACTIONS from "../../Actions/actionTypes";
 const initialState = {
   loading: false,
 };
 
 const Friendships = (state = initialState, { type, payload }) => {
   switch (type) {
+    case ACTIONS.FRIENDSHIP_LOADING: {
+      return { ...state, loading: !state.loading };
+    }
+    case ACTIONS.ADD_FRIENDSHIP: {
+      const friendship = payload.friendship;
+      return { ...state, ...friendship };
+    }
     default:
       return state;
   }

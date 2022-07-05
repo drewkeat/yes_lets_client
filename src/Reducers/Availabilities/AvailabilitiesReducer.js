2764890b7a53470386c27bcbe15d1e18998ba2af
Drew Keat <thedrewkeat@gmail.com>
2022-07-04 22:09:19 -0600

    Build Destroy_Availability in AvailabilitiesReducer

--- src/Reducers/Availabilities/AvailabilitiesReducer.js
+++ src/Reducers/Availabilities/AvailabilitiesReducer.js
@@ -1,3 +1,4 @@
+import _ from "lodash";
 import ACTIONS from "../../Actions/actionTypes";
 const initialState = {
   loading: false,
@@ -15,8 +16,9 @@ const Availabilities = (state = initialState, { type, payload }) => {
     case ACTIONS.DESTROY_AVAILABILITY: {
       // const availability = payload.availability;
       // TODO: Handle logic for removing this from state
-      debugger;
-      return { ...state };
+      const newState = { ...state };
+      delete newState[_.keys(payload.availability)[0]];
+      return { ...newState };
     }
     default:
       return state;
